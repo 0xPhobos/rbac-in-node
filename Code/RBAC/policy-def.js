cbaef16cc2b9aec3054d150ebe1ddc31323ac691
Phobos <86452055+0xPhobos@users.noreply.github.com>
2022-02-06 01:05:21 -0500

    Create policy-def.js

---
+++ Code/RBAC/policy-def.js
@@ -0,0 +1,13 @@
+const { RBAC } = require("rbac");
+const policy = new RBAC({
+  roles: ["clone", "sidekick", "evilGenius"],
+  permissions: {
+    megaSeeds: ["gather", "consume", "destroy"],
+    timeCrystals: ["gather", "consume", "destroy"],
+  },
+  grants: {
+    clone: ["gather_megaSeeds", "gather_timeCrystals"],
+    sidekick: ["clone", "consume_megaSeeds", "consume_timeCrystals"],
+    evilGenius: ["sidekick", "destroy_megaSeeds", "destroy_timeCrystals"],
+  },
+});
