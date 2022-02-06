f153060a7443550c8f9391d8230f9b12197c9b43
Phobos <86452055+0xPhobos@users.noreply.github.com>
2022-02-06 01:04:25 -0500

    Create declarativesyntax-define-abilities.js

---
+++ Code/CASL/declarativesyntax-define-abilities.js
@@ -0,0 +1,34 @@
+import { AbilityBuilder, Ability } from "@casl/ability";
+import { resolveUserRoles } from "../utils.js";
+
+export function defineRulesFor(user) {
+  const { can, rules } = new AbilityBuilder(Ability);
+
+  // If no user, no rules
+  if (!user) return new Ability(rules);
+  const roles = resolveUserRoles(user);
+
+  roles.forEach((role) => {
+    switch (role) {
+      case "clone":
+        can("gather", "Asset", { id: "megaSeeds" });
+        can("gather", "Asset", { id: "timeCrystals" });
+        break;
+      case "sidekick":
+        can("gather", "Asset", { id: "megaSeeds" });
+        can("gather", "Asset", { id: "timeCrystals" });
+        can("consume", "Asset", { id: "timeCrystals" });
+        can("consume", "Asset", { id: "megaSeeds" });
+        break;
+      case "evilGenius":
+        can("manage", "all");
+        break;
+      default:
+        // anonymous users can't do anything
+        can();
+        break;
+    }
+  });
+
+  return new Ability(rules);
+}
